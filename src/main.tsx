ea69088b417edd2a4fe07b2b1cda4cc52329d036
TMB <146067045+toluwanibakare@users.noreply.github.com>
2026-08-27 09:44:09 +0100

    Fix unused StrictMode import failing Vercel build

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { WagmiProvider } from 'wagmi'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
