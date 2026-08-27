7da73b71a2ad680a65461b89cd1022588365fd53
TMB <146067045+toluwanibakare@users.noreply.github.com>
2026-08-27 12:12:40 +0100

    Force RainbowKit to immediately switch to Hashkey Testnet on mobile

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { createRoot } from 'react-dom/client'
 import { WagmiProvider } from 'wagmi'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { config } from './lib/wagmi'
+import { config, hskTestnet } from './lib/wagmi'
 import './index.css'
 import App from './App'
 
@@ -19,6 +19,7 @@ createRoot(document.getElementById('root')!).render(
     <QueryClientProvider client={queryClient}>
       <RainbowKitProvider 
         theme={localStorage.getItem('runda-dark') === 'true' ? darkTheme() : lightTheme()}
+        initialChain={hskTestnet}
       >
         <App />
       </RainbowKitProvider>
